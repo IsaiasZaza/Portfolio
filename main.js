afc3335027638985615e4137aded5c566e1bab50
Isaias Gonçalves <isaias.legend@gmail.com>
2023-12-04 16:57:56 -0300

    feat no design do portfolio

---
+++ main.js
@@ -0,0 +1,29 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('#menu');
+
+sr.reveal('#sobre-Mim', {duration: 2000});
+
+sr.reveal('.tamanhoHard', {duration: 2000});
+
+sr.reveal('.textoAparecer', {duration: 2000})
+
+
+//imagens hard-skills
+sr.reveal('.logo-1', {duration: 3000})
+sr.reveal('.logo-2', {duration: 3500})
+sr.reveal('.logo-3', {duration: 4000})
+sr.reveal('.logo-4', {duration: 4500})
+sr.reveal('.logo-5', {duration: 5000})
+sr.reveal('.logo-6', {duration: 5500})
+sr.reveal('.logo-7', {duration: 6000})
+sr.reveal('.logo-8', {duration: 6500})
+sr.reveal('.logo-9', {duration: 7000})
+
+//projetosFinais
+
+sr.reveal('.isso')
+sr.reveal('.projeto-01', {duration: 3000});
+sr.reveal('.projeto-02', {duration: 3500});
+sr.reveal('.projeto-03', {duration: 4000});
+sr.reveal('.projeto-04', {duration: 4500});
